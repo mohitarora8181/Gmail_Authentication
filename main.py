5072cb9cd7bac81a3bccddad9ce620222aeefc40
Rakesh Arora <mannyisha808@gmail.com>
2024-07-23 00:48:55 +0530

    firstTimePython:)

---
+++ main.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI,status,HTTPException,Response
+from fastapi.responses import JSONResponse
+from dotenv import load_dotenv
+from datetime import datetime,timedelta
+from typing import Dict
+from jose import jwt,JWTError
+from pymongo import MongoClient
+from sendMail import send_with_template
+
+import os
+
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client["users"].verifyData
+
+
+app = FastAPI()
+
+@app.post("/login")
+def login(email:str,response:Response):
+    user = db.find_one({"email":email})
+    if(not user):
+        response.status_code=status.HTTP_404_NOT_FOUND
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="No user found")
+    if(not db.find_one({"access_token":{"$exists":True}})):
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Unauthorized")
+    
+    decoded_user = jwt.decode(user["access_token"],os.getenv("JWT_SECRET"),algorithms="HS256")
+    if(decoded_user["exp"] > int(round(datetime.now().timestamp()))):
+        response.status_code = status.HTTP_200_OK
+        return HTTPException(status_code=status.HTTP_200_OK,detail="Successful login")
+    
+    response.status_code = status.HTTP_401_UNAUTHORIZED
+    return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Unauthorized")
+
+
+@app.post("/generate_token")
+def start(data:Dict):
+    info = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=2)
+    info.update({"exp":expire})
+    token = jwt.encode(info,os.getenv("JWT_SECRET"),algorithm="HS256")
+
+    isVerifiedUser = db.find_one({"email":data["email"]})
+    if(isVerifiedUser and isVerifiedUser["exp"] > int(round(datetime.now().timestamp()))):
+        return JSONResponse({"success":False,"message":"Link is already sent to your gmail , new link will be generated after 2 mins."},status_code=503)
+    elif(isVerifiedUser):
+        db.update_one({"email":data["email"]},{"$set":{"exp":info["exp"]}})
+    else:
+        db.insert_one(info)
+    return JSONResponse({"token":token})
+
+    
+@app.post("/sendMail")
+async def sendMail(token:str):
+    await send_with_template(token)
+    return True
+
+
+@app.get("/verify")
+def verify(token:str):
+    try:
+        user = jwt.decode(token,os.getenv("JWT_SECRET"),algorithms="HS256")
+        expire = datetime.utcnow() + timedelta(hours=24)
+        user.update({"exp":expire})
+        token = jwt.encode(user,os.getenv("JWT_SECRET"),algorithm="HS256")
+        db.update_one({"email":user["email"]},{"$set":{"access_token":token}})
+
+        return True
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Timeout , generate new verify link again"
+        )
---
+++ sendMail.py
@@ -0,0 +1,106 @@
+from fastapi_mail import FastMail,MessageSchema,ConnectionConfig,MessageType
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conf = ConnectionConfig(
+    MAIL_USERNAME = os.getenv("GMAIL_USERNAME"),
+    MAIL_PASSWORD = os.getenv("GMAIL_PASS"),
+    MAIL_FROM = os.getenv("GMAIL_USERNAME"),
+    MAIL_PORT = 587,
+    MAIL_SERVER = "smtp.gmail.com",
+    MAIL_FROM_NAME="FixIt-AI",
+    MAIL_STARTTLS = True,
+    MAIL_SSL_TLS = False,
+    USE_CREDENTIALS = True,
+    VALIDATE_CERTS = True
+)
+
+async def send_with_template(token:str):
+
+    html = f"""<!DOCTYPE html>
+        <html>
+        <head>
+            <style>
+                body {{
+                    font-family: Arial, sans-serif;
+                    line-height: 1.6;
+                    color: #333333;
+                    background-color: #f7f7f7;
+                    margin: 0;
+                    padding: 0;
+                }}
+                .container {{
+                    max-width: 600px;
+                    margin: 20px auto;
+                    background-color: #ffffff;
+                    border-radius: 10px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    border:1px solid black;
+                }}
+                .header {{
+                    text-align: center;
+                    padding: 10px 0;
+                    background-color: #000000;
+                    color: #ffffff;
+                    border-top-left-radius: 10px;
+                    border-top-right-radius: 10px;
+                }}
+                .header h1 {{
+                    margin: 0;
+                    font-size: 24px;
+                }}
+                .content {{
+                    padding: 20px;
+                }}
+                .content p {{
+                    margin: 10px 0;
+                }}
+                .button {{
+                    display: block;
+                    width: 200px;
+                    margin: 20px auto;
+                    padding: 10px;
+                    text-align: center;
+                    background-color: #000000;
+                    color: white;
+                    text-decoration: none;
+                    border-radius: 5px;
+                }}
+                .footer {{
+                    text-align: center;
+                    padding: 10px 0;
+                    color: #777777;
+                    font-size: 12px;
+                }}
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="header">
+                    <h1>Verify Your Email Address</h1>
+                </div>
+                <div class="content">
+                    <p>Thank you for registering with us. Please verify your email address by clicking the button below:</p>
+                    <a style={{color:"white"}} href="http://127.0.0.1:8000/verify?token={ token }" class="button">Verify Account</a>
+                    <p>If you did not create an account, no further action is required.</p>
+                    <p>Best regards,</p>
+                    <p>Fixit AI</p>
+                </div>
+                <div class="footer">
+                    <p>&copy; 2024 Your Company Name. All rights reserved.</p>
+                </div>
+            </div>
+        </body>
+        </html>
+        """
+    message = MessageSchema(
+        subject="Verify your identity",
+        recipients=["mohitarora00777@gmail.com"],
+        body=html,
+        subtype=MessageType.html,
+        )
+
+    fm = FastMail(conf)
+    await fm.send_message(message) 
